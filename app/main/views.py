112d8022b4d9842d8dab7775d28dc5899ad4d4d0
Geerocktricks <geerockface4@gmail.com>
2018-11-13 23:56:55 +0300

    Second deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -25,9 +25,9 @@ def articles(id):
     '''
     view source page function that returns the source details page and its data
     '''
-    message = 'GERALD mag.'
-    Trending = 'Trending'
+    # message = 'GERALD mag.'
+    # Trending = 'Trending'
     title = 'Gerald mag...'
     articles = get_articles(id)
 
-    return render_template('index.html' ,  message = message ,'source.html',articles=articles , Trending = Trending , title = title)    
+    return render_template('source.html',articles=articles ,title = title)    
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-export MOVIE_API_KEY=98dea97de1bb43c8bbdbf37abaa9d66b
-export SECRET_KEY=<1921
-
+export NEWS_API_KEY=98dea97de1bb43c8bbdbf37abaa9d66b
+export SECRET_KEY= 1921
 
 python3.6 manage.py server
